3cfe4a6a991c7782d6342f8cb50a75971be5e0f9
Tom <73077675+junk1tm@users.noreply.github.com>
2021-12-15 01:14:52 +0300

    feat: use semver regexp (#4)

--- README.md
+++ README.md
@@ -11,13 +11,90 @@ A dead simple CLI tool that prints the next semantic version based on the last t
 
 ## Install
 
+### Go
+
 ```bash
 go install github.com/junk1tm/nextver@latest
 ```
 
+### Manual
+
+Download a prebuilt binary from the [Releases][release] page.
+
 ## Usage
 
+`nextver` supports `major`, `minor` and `patch` commands:
+
 ```bash
+> git tag
+1.2.3
+> nextver major
+2.0.0
+> nextver minor
+1.3.0
+> nextver patch
+1.2.4
+```
+
+`nextver` expects an initiated `git` repository with tags following the [semantic versioning specification][semver],
+invalid tags are ignored:
+
+```bash
+> git tag
+a.b.c
+1.2.3.4
+> nextver -verbose patch
+nextver: skipping "a.b.c": invalid semantic version format
+nextver: skipping "1.2.3.4": invalid semantic version format
+nextver: no valid semantic version has been found
+```
+
+`nextver` is designed to be used with `git tag` command:
+
+```bash
+git tag $(nextver patch)
+```
+
+so the only thing it writes to `stdout` is the next version, everything else, including usage, errors and additional
+information, goes to `stderr`. If anything goes wrong, e.g., no `git` repository is found, `nextver` won't
+touch `stdout` at all, so the command above will be resolved to `git tag`, which simply prints the existing `git` tags.
+Therefore, accidentally creating an invalid tag is unlikely.
+
+### Prefix
+
+The `-prefix` flag allows defining a prefix for tags. If set, `nextver` ignores tags without this prefix and prints the
+next version prefixed.
+
+A common practice is to prefix `semver` tags with `v`:
+
+```bash
+> git tag
+v1.2.3
+> nextver -prefix=v patch
+v1.2.4
+```
+
+Another use case is working with [go submodules][submodule], which requires submodule's tags to be prefixed:
+
+```bash
+> tree mainmodule
+mainmodule
+├── go.mod
+└── submodule
+    └── go.mod
+
+1 directory, 2 files
+> cd mainmodule && git tag
+0.1.0
+submodule/0.1.1
+> nextver -prefix=submodule/ patch
+submodule/0.1.2
+```
+
+### Help
+
+```bash
+> nextver -help
 usage: nextver [flags] <command>
 
 available commands:
@@ -53,3 +130,5 @@ available flags:
 [license-img]: https://img.shields.io/github/license/junk1tm/nextver
 [release]: https://github.com/junk1tm/nextver/releases
 [release-img]: https://img.shields.io/github/v/release/junk1tm/nextver
+[semver]: https://semver.org/#semantic-versioning-specification-semver
+[submodule]: https://github.com/go-modules-by-example/index/blob/master/009_submodules/README.md
--- version.go
+++ version.go
@@ -3,14 +3,14 @@ package main
 import (
 	"errors"
 	"fmt"
+	"regexp"
 	"strconv"
-	"strings"
 )
 
-var (
-	errInvalidFormat  = errors.New("semantic version must have exactly 3 numbers")
-	errNegativeNumber = errors.New("all semantic version numbers must be non-negative")
-)
+var errInvalidFormat = errors.New("invalid semantic version format")
+
+// https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+var semver = regexp.MustCompile(`^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$`)
 
 // version is a parsed semantic version.
 type version struct {
@@ -20,28 +20,27 @@ type version struct {
 }
 
 // parseVersion parses a semantic version from the provided string.
-// The string must take the form "x.y.z", where x, y and z are non-negative integers.
-// No "v" or any other prefix is expected.
-// See https://semver.org/#semantic-versioning-specification-semver for details.
-func parseVersion(s string) (version, error) {
-	parts := strings.Split(s, ".")
-	if len(parts) != 3 {
-		return version{}, errInvalidFormat
+// The string must meet the semantic versioning specification requirements,
+// see https://semver.org/#semantic-versioning-specification-semver for details.
+func parseVersion(s string) (*version, error) {
+	const minMatches = 1 + 3 // 1 full match + at least 3 subexpression matches.
+
+	matches := semver.FindStringSubmatch(s)
+	if len(matches) < minMatches {
+		return nil, errInvalidFormat
 	}
 
 	var numbers []int
-	for _, p := range parts {
-		n, err := strconv.Atoi(p)
+	for _, match := range matches[1:minMatches] {
+		n, err := strconv.Atoi(match)
 		if err != nil {
-			return version{}, fmt.Errorf("parsing number: %w", err)
-		}
-		if n < 0 {
-			return version{}, errNegativeNumber
+			// unreachable due to the semver regexp.
+			panic(err)
 		}
 		numbers = append(numbers, n)
 	}
 
-	return version{
+	return &version{
 		major: numbers[0],
 		minor: numbers[1],
 		patch: numbers[2],
@@ -50,8 +49,7 @@ func parseVersion(s string) (version, error) {
 
 const format = "%d.%d.%d"
 
-func (v version) String() string    { return fmt.Sprintf(format, v.major, v.minor, v.patch) }
-func (v version) nextMajor() string { return fmt.Sprintf(format, v.major+1, 0, 0) }
-func (v version) nextMinor() string { return fmt.Sprintf(format, v.major, v.minor+1, 0) }
-func (v version) nextPatch() string { return fmt.Sprintf(format, v.major, v.minor, v.patch+1) }
-func (v version) isZero() bool      { return v.major == 0 && v.minor == 0 && v.patch == 0 }
+func (v *version) String() string    { return fmt.Sprintf(format, v.major, v.minor, v.patch) }
+func (v *version) nextMajor() string { return fmt.Sprintf(format, v.major+1, 0, 0) }
+func (v *version) nextMinor() string { return fmt.Sprintf(format, v.major, v.minor+1, 0) }
+func (v *version) nextPatch() string { return fmt.Sprintf(format, v.major, v.minor, v.patch+1) }
